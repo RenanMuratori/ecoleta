107c3e83910570cb9baf437de168593585ddcfdd
Renan Muratori <renan.muratori@gmail.com>
2020-06-09 18:53:34 -0300

    Primeiro commit. Conteúdo até a segunda aula.

---
+++ src/pages/CreatePoint/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
+
+const CreatePoint = () => {
+    return (
+        <div id="id-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta"/>
+
+                <Link to="/">
+                    Voltar para home
+                </Link>
+            </header>
+        </div>
+
+    );
+}
+
+export default CreatePoint;
